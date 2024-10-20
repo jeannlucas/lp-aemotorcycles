44fbf03770e0cc590d2c90ba3adecacdadd8760c
Jeann Lucas <jeann.lucas@outlook.com>
2024-10-20 13:14:47 -0300

    chore: remove unnecessary comments

--- assets/scripts/category.js
+++ assets/scripts/category.js
@@ -1,30 +1,28 @@
 import { axiosInstance } from "../lib/api.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  try {
-    const response = await axiosInstance.get("/categories");
+      try {
+            const response = await axiosInstance.get("/categories");
 
-    // Em vez de verificar `response.ok`, verificamos o status diretamente
-    if (response.status !== 200) {
-      throw new Error("Network response was not ok");
-    }
+            if (response.status !== 200) {
+                  throw new Error("Network response was not ok");
+            }
 
-    // A resposta já está no formato `data` em Axios
-    const categories = response.data;
+            const categories = response.data;
 
-    const portfolioFilters = document.getElementById("portfolio-flters");
+            const portfolioFilters = document.getElementById("portfolio-flters");
 
-    portfolioFilters
-      .querySelectorAll("li:not(.filter-active)")
-      .forEach((item) => item.remove());
+            portfolioFilters
+                  .querySelectorAll("li:not(.filter-active)")
+                  .forEach((item) => item.remove());
 
-    categories.forEach((category) => {
-      const li = document.createElement("li");
-      li.textContent = category.name;
-      li.setAttribute("data-filter", `.filter-${category.id}`);
-      portfolioFilters.appendChild(li);
-    });
-  } catch (error) {
-    console.error("Failed to fetch categories:", error);
-  }
+            categories.forEach((category) => {
+                  const li = document.createElement("li");
+                  li.textContent = category.name;
+                  li.setAttribute("data-filter", `.filter-${category.id}`);
+                  portfolioFilters.appendChild(li);
+            });
+      } catch (error) {
+            console.error("Failed to fetch categories:", error);
+      }
 });
--- assets/scripts/product-details.js
+++ assets/scripts/product-details.js
@@ -1,18 +1,16 @@
 import { axiosInstance } from "../lib/api.js";
 document.addEventListener("DOMContentLoaded", function () {
-  const params = new URLSearchParams(window.location.search);
-  const productId = params.get("id");
-  if (!productId) {
-    console.error("ID do produto não encontrado na URL");
-    return;
-  }
-  async function loadProductDetails() {
-    try {
-      // Requisição para buscar os detalhes do produto
-      const response = await axiosInstance.get(`/products/${productId}`);
-      const product = response.data;
-      // Atualizar as informações do produto na página
-      document.querySelector(".portfolio-info ul").innerHTML = `
+      const params = new URLSearchParams(window.location.search);
+      const productId = params.get("id");
+      if (!productId) {
+            console.error("ID do produto não encontrado na URL");
+            return;
+      }
+      async function loadProductDetails() {
+            try {
+                  const response = await axiosInstance.get(`/products/${productId}`);
+                  const product = response.data;
+                  document.querySelector(".portfolio-info ul").innerHTML = `
 <h3>${product.name}</h3>
 <li><strong>Categoria</strong>: ${product.category.name}</li>
 <li><strong>Marca</strong>: ${product.brand.name}</li>
@@ -21,48 +19,47 @@ document.addEventListener("DOMContentLoaded", function () {
 <li class="preco-atacado"><strong>Preço Atacado</strong>: <a href="https://wa.me/5544999300014?text=Olá, tenho interesse em realizar compras no atacado. Poderia me orientar sobre os passos que devo seguir?" target="_blank">
    Sob Consulta</a>
 </li>
-<li class="anuncio-ml"><strong>Anúncio no Mercado Livre</strong>: <a href="${
-        product.mercado_livre
-      }" target="_blank">Clique aqui</a></li>
+<li class="anuncio-ml"><strong>Anúncio no Mercado Livre</strong>: <a href="${product.mercado_livre
+                        }" target="_blank">Clique aqui</a></li>
 `;
-      document.querySelector(".portfolio-description p").textContent =
-        product.description;
-      await loadProductImages(product.id);
-    } catch (error) {
-      console.error("Erro ao carregar os detalhes do produto:", error);
-    }
-  }
-  async function loadProductImages(productId) {
-    try {
-      const response = await axiosInstance.get(`/products/images/${productId}`);
-      const images = response.data;
-      const sliderWrapper = document.querySelector(
-        ".portfolio-details-slider .swiper-wrapper"
-      );
-      sliderWrapper.innerHTML = "";
-      images.forEach((imageData) => {
-        const slide = document.createElement("div");
-        slide.classList.add("swiper-slide");
-        slide.innerHTML = `<img src="data:image/jpeg;base64,${imageData.image}" alt="Imagem do Produto">`;
-        sliderWrapper.appendChild(slide);
-      });
-      initializeSwiper();
-    } catch (error) {
-      console.error("Erro ao carregar as imagens do produto:", error);
-    }
-  }
-  function initializeSwiper() {
-    new Swiper(".portfolio-details-slider", {
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-    });
-  }
-  loadProductDetails();
+                  document.querySelector(".portfolio-description p").textContent =
+                        product.description;
+                  await loadProductImages(product.id);
+            } catch (error) {
+                  console.error("Erro ao carregar os detalhes do produto:", error);
+            }
+      }
+      async function loadProductImages(productId) {
+            try {
+                  const response = await axiosInstance.get(`/products/images/${productId}`);
+                  const images = response.data;
+                  const sliderWrapper = document.querySelector(
+                        ".portfolio-details-slider .swiper-wrapper"
+                  );
+                  sliderWrapper.innerHTML = "";
+                  images.forEach((imageData) => {
+                        const slide = document.createElement("div");
+                        slide.classList.add("swiper-slide");
+                        slide.innerHTML = `<img src="data:image/jpeg;base64,${imageData.image}" alt="Imagem do Produto">`;
+                        sliderWrapper.appendChild(slide);
+                  });
+                  initializeSwiper();
+            } catch (error) {
+                  console.error("Erro ao carregar as imagens do produto:", error);
+            }
+      }
+      function initializeSwiper() {
+            new Swiper(".portfolio-details-slider", {
+                  loop: true,
+                  pagination: {
+                        el: ".swiper-pagination",
+                        clickable: true,
+                  },
+                  autoplay: {
+                        delay: 5000,
+                        disableOnInteraction: false,
+                  },
+            });
+      }
+      loadProductDetails();
 });
